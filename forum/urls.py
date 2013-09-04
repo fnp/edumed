1e0e7cfcbfdf5b88839397b1a9a52f3433bff2cf
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-09-04 13:35:03 +0200

    Forum search

--- forum/urls.py
+++ forum/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import patterns, include, url
+from haystack.query import SearchQuerySet
+from haystack.views import SearchView, search_view_factory
+from haystack.forms import SearchForm
+from pybb.models import Post
 
 from .views import AddPostView, EditPostView
 
@@ -7,3 +11,13 @@ urlpatterns = patterns('',
     url(r'^forum/(?P<forum_id>\d+)/topic/add/$', AddPostView.as_view()),
     url(r'^post/(?P<pk>\d+)/edit/$', EditPostView.as_view()),
 )
+
+PostsSearchQuerySet = SearchQuerySet().models(Post).highlight()
+
+urlpatterns += patterns('haystack.views',
+    url(r'^szukaj/$', search_view_factory(
+        view_class = SearchView,
+        template = 'forum/search_results.html',
+        searchqueryset = PostsSearchQuerySet,
+        form_class = SearchForm
+    ), name='forum_search'))
