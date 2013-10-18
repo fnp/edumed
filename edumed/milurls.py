ca473e6f63c32db5e5f8abf52ef72a8d03f25c45
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-10-18 09:42:15 +0200

    Use Django flatpages mechanism for mil home page

--- edumed/milurls.py
+++ edumed/milurls.py
@@ -1,13 +1,17 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 
 from fnpdjango.utils.urls import i18n_patterns
 from .views import MILHomeView
 
 
 urlpatterns = i18n_patterns('',
-    url(r'^$', MILHomeView.as_view(), name="mil_home"),
     url(r'^kompetencje/', include('curriculum.urls')),
     url(r'^wez-udzial/', include('comment.urls'))
+)
+
+urlpatterns += patterns('',
+    url(r'^$', 'django.contrib.flatpages.views.flatpage', {'url': '/'}, name="mil_home_pl"),
+    url(r'^en/$', 'django.contrib.flatpages.views.flatpage', {'url': '/en/'}, name="mil_home_en")
 )
 
 handler404 = 'edumed.views.mil_404_view'
