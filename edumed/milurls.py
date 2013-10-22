11707f7fdcde7ae55471c9c8b4be29acb409e01b
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-10-22 09:48:34 +0200

    kontakt/contact page accessible under katalog.* domain.

--- edumed/milurls.py
+++ edumed/milurls.py
@@ -11,6 +11,13 @@ urlpatterns = i18n_patterns('',
     url(r'^zglos/', include('contact.urls')),
 )
 
+urlpatterns += patterns('',
+    url(r'^kontakt/', 'django.contrib.flatpages.views.flatpage', {'url': 'kontakt/'},
+        name="info_contact_pl"),
+    url(r'^contact/', 'django.contrib.flatpages.views.flatpage', {'url': 'contact/'},
+        name="info_contact_en"),
+)
+
 handler404 = 'edumed.views.mil_404_view'
 
 from django.conf import settings
