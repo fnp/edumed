ca5ba2a8976e5401291150a0c3fbc9b1a3fb5f95
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-11-06 12:36:13 +0100

    Submission string representation in admin

--- wtem/models.py
+++ wtem/models.py
@@ -20,6 +20,8 @@ class Submission(models.Model):
     email = models.EmailField(max_length = 100, unique = True)
     answers = models.CharField(max_length = 65536, null = True, blank = True)
     key_sent = models.BooleanField(default = False)
+    def __unicode__(self):
+        return ', '.join((self.last_name, self.first_name, self.email))
 
     @classmethod
     def generate_key(cls):
