8f939c57adbcfa3bb6f2ab9625881495140c4501
Radek Czajka <radekczajka@nowoczesnapolska.org.pl>
2014-01-31 16:00:33 +0100

    Oh, right

---
+++ publishers/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='publishers/logo')
+
+    def __unicode__(self):
+        return self.name
