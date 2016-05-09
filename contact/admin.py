30574b902abfedf033f7c71e65f8152a53b5cccc
Jan Szejko <jan.szejko@gmail.com>
2016-05-09 16:43:29 +0200

    csv export: support for bools and lists

--- contact/admin.py
+++ contact/admin.py
@@ -157,6 +157,11 @@ def extract_view(request, form_tag, extract_type_slug):
                 for key in keys:
                     if key not in record:
                         record[key] = ''
+                    if isinstance(record[key], bool):
+                        record[key] = 'tak' if record[key] else 'nie'
+                    if isinstance(record[key], (list, tuple)):
+                        record[key] = '; '.join(record[key])
+
                 csv_writer.writerow([record[key] for key in keys])
         csv_writer.writerow([])
 
