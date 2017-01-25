bcebd10994f5e22f23bb6d2dc7cd4c7889dbdbfe
Jan Szejko <janek37@gmail.com>
2017-01-25 17:16:58 +0100

    option to exclude lessons by slugs

--- catalogue/management/commands/republish.py
+++ catalogue/management/commands/republish.py
@@ -2,17 +2,34 @@
 # This file is part of EduMed, licensed under GNU Affero GPLv3 or later.
 # Copyright © Fundacja Nowoczesna Polska. See NOTICE for more information.
 #
+from optparse import make_option
+
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = 'Republishes all lessons.'
 
+    option_list = BaseCommand.option_list + (
+        make_option('--exclude', dest='exclude', metavar="PATH", default=None,
+                    help='PATH to file with excluded lesson slugs.'),
+        make_option('--ignore-incomplete', action='store_true', dest='ignore_incomplete', default=False,
+                    help='Attachments dir path.'),
+        make_option('--dont-repackage', action='store_false', dest='repackage', default=True,
+                    help='Don\'t refresh level packages.'),
+    )
+
     def handle(self, **options):
         from catalogue.models import Lesson
         from curriculum.models import Level
 
-        for lesson in Lesson.objects.all():
+        lessons = Lesson.objects.order_by('slug')
+
+        if options.get('exclude'):
+            slugs = [line.strip() for line in open(options['exclude'])]
+            lessons = lessons.exclude(slug__in=slugs)
+
+        for lesson in lessons:
             print
             print 'Republishing: %s' % lesson.slug
             # try:
