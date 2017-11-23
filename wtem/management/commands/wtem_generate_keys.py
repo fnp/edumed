04f061ccd6a7f64283d2144edafb088b7c3caf30
Jan Szejko <janek37@gmail.com>
2017-11-23 11:29:14 +0100

    very minor fix

--- wtem/management/commands/wtem_generate_keys.py
+++ wtem/management/commands/wtem_generate_keys.py
@@ -12,14 +12,10 @@ class Command(BaseCommand):
 
         emails = list(Submission.objects.values_list('email', flat=True))
         for confirmation in Confirmation.objects.exclude(email__in=emails):
-            if not Submission.objects.filter(email=confirmation.email).exists():
-                args = {}
-                for attr in ['first_name', 'last_name', 'email', 'contact']:
-                    args[attr] = getattr(confirmation, attr)
-                Submission.create(**args)
-                new += 1
-            else:
-                self.stdout.write('skipping ' + confirmation.email + ': already exists.')
-                skipped += 1
+            args = {}
+            for attr in ['first_name', 'last_name', 'email', 'contact']:
+                args[attr] = getattr(confirmation, attr)
+            Submission.create(**args)
+            new += 1
 
         self.stdout.write('New: ' + str(new) + ', skipped: ' + str(skipped))
